c47770258a1b1fd83ca78121fdcb1a300b389f01
sdhutchinso <xinhuangguai242@yeah.net>
2021-06-19 08:09:55 +0800

    增加构造方法

--- src/cn/bvin/lib/module/update/DBUpdateInfo.java
+++ src/cn/bvin/lib/module/update/DBUpdateInfo.java
@@ -2,6 +2,10 @@ package cn.bvin.lib.module.update;
 
 public class DBUpdateInfo extends UpdateInfo{
 
+	public DBUpdateInfo(String version, String updateDesc, String apkUrl,
+			String apkFileSize) {
+		super(version, updateDesc, apkUrl, apkFileSize);
+	}
 	public String dbVersion;// 基础数据版本
 	public String dbFileUrl;// 基础数据地址 
 }
--- src/cn/bvin/lib/module/update/UpdateInfo.java
+++ src/cn/bvin/lib/module/update/UpdateInfo.java
@@ -1,7 +1,23 @@
 package cn.bvin.lib.module.update;
 
 public class UpdateInfo {
-
+	
+	/**
+	 * 检测更新信息
+	 * @param version 版本号
+	 * @param updateDesc 更新内容
+	 * @param apkUrl 更新文件地址
+	 * @param apkFileSize 更新文件大小
+	 */
+	public UpdateInfo(String version, String updateDesc, String apkUrl,
+			String apkFileSize) {
+		super();
+		this.version = version;
+		this.updateDesc = updateDesc;
+		this.apkUrl = apkUrl;
+		this.apkFileSize = apkFileSize;
+	}
+	
 	public String version;// 版本号
 	public String updateDesc;// 更新内容
 	public String apkUrl;// 更新文件地址
